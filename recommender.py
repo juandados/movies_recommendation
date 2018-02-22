0d3f6372f2de1cefe436ed28fa7f58d895e9574c
Juan <juandados@gmail.com>
2018-02-22 02:20:08 -0500

    debugging python script

--- recommender.py
+++ recommender.py
@@ -58,62 +58,58 @@ class MovieRecommender:
         cls.movies_df = movies_df
 
     @classmethod
-    def fit(cls):
+    def matrix_decomposition(cls):
         '''
         It determines the matrix descomposition (sigular value matrix descomposition), which will be use to stimate ratings
         '''
-        R_df = cls.ratings_df.pivot(index="userId", columns="movieId", values="rating").fillna(0)
-        cls.users_index = R_df.index.values
-        cls.movies_index = R_df.columns.values
-        cls.U, cls.sigma, cls.Vt = svds(R_df.as_matrix(),k=np.min([(np.min(R_df.shape)-1),8]))
+        cls.R_df = cls.ratings_df.pivot(index="userId", columns="movieId", values="rating").fillna(-1)
+        cls.users_index = cls.R_df.index.values
+        cls.movies_index = cls.R_df.columns.values
+        cls.U, cls.sigma, cls.Vt = svds(cls.R_df.as_matrix(),k=np.min([(np.min(cls.R_df.shape)-1),8]))
 
     def __init__(self, userId):
         self.userId = userId
 
 
-    def recommend(self,limit=4):
+    def recommend(self,recommendations_limit=4, historical_limit=10):
         '''
         It makes a specified number of recommendations, based on higher estimated ratings
         '''
+
+        '''
         cls = self.__class__
-        seen_movies = cls.ratings_df.loc[cls.ratings_df["userId"] == self.userId]["movieId"]
-        estimated_ratings = self.estimate_ratings()
-        not_seen_estimated_ratings = estimated_ratings.loc[estimated_ratings["movieId"].map(lambda x: not x in seen_movies.values)]
-        top_recommendations = not_seen_estimated_ratings.sort_values("estimated_rating",ascending=False).head(limit)
-        top_recommendations = pd.merge(top_recommendations,cls.movies_df, on = "movieId")
-        del top_recommendations["estimated_rating"]
+        print(cls.R_df)
+        ratings = cls.R_df.loc[self.userId]
+        self.best_ranked_movies = ratings[ratings>0.7].sort_values(ascending=False).head(historical_limit)
+        ## Selecting Features for unseen movies
+        Vt_df = pd.DataFrame(Vt,columns=R_df.columns)
+        unseen_movies = Vt_df[self.R_df.columns[ratings == -1]]
+        ### Calculating Distances over Vt Matrix
+        differences = []
+        for movie in Vt_df[self.best_ranked_movies.index].items():
+            differences.append(unseen_movies.apply(lambda x: norm(x-movie[1])).values)
+        distances = pd.DataFrame(differences,columns=unseen_movies.columns)
+        top_recommendations = list(distances.min().sort_values().head(limit).index)
         return top_recommendations
+        '''
+        return "pass"
 
-    def estimate_ratings(self):
-        '''
-        It estimated the rating for every movie in the original rating list, based on the SVD factorization
-        '''
-        cls = self.__class__
-        estimated_ratings = np.dot(cls.U[self.userId-1],np.dot(np.diag(cls.sigma),cls.Vt))
-        estimated_ratings_df = pd.DataFrame([cls.movies_index,estimated_ratings],index=["movieId","estimated_rating"]).T
-        return estimated_ratings_df
-
-    def get_top_historical_ratings(self, limit=10):
-        '''
-        It get the limit-top historical rating for a specific user
-        '''
-        cls = self.__class__
-        ratings = cls.ratings_df.loc[cls.ratings_df["userId"] == self.userId]
-        top_historical_ratings = ratings.sort_values("rating",ascending = False)
-        top_historical_ratings = pd.merge(top_historical_ratings.head(limit),cls.movies_df, on="movieId")
-        return top_historical_ratings
-
+    def get_top_historical_ratings(self):
+        return self.best_ranked_movies
 
 if __name__ == '__main__':
     try:
         names = ["Dennis+Schwartz","James+Berardinelli","Scott+Renshaw","Steve+Rhodes"]
+        # Extracting titles from reviews
         for name in names:
             data_loader = DataLoader(name)
             data_loader.filling_reviews_titles()
+        ## Creating movies DataFrame
         movies_df = pd.DataFrame([DataLoader.review_titles],index=["movieId"]).T
         movies_df.reset_index(inplace=True)
         movies_df.rename(columns={"index":"reviewId"},inplace=True) 
         movies_df["title"] = movies_df["movieId"]
+        ## Creating a ratings DataFrame
         id_base_dir = "./cornell-data/scale_data/scaledata/{}/id.{}"
         rating_base_dir = "./cornell-data/scale_data/scaledata/{}/rating.{}"
         ratings = pd.DataFrame([],columns=["reviewId","rating","userId"])
@@ -124,24 +120,31 @@ if __name__ == '__main__':
             user_ratings["userId"] = name
             ratings = pd.concat([ratings,user_ratings])
         ratings_df = pd.merge(ratings,movies_df, on="reviewId")
+        ### Droping duplicated reviews for a single user
         ratings_df = ratings_df.groupby(["userId","movieId"]).head(1)
 
         inputs = json.loads(sys.argv[1])
         # Load the data to the MovieRecommender class
         MovieRecommender.load_data_set(ratings_df = ratings_df, movies_df = movies_df)
-        # Fit the recommendation engine 
-        MovieRecommender.fit()
+        # Making Lower Rank Decomposition 
+        MovieRecommender.matrix_decomposition()
+
         # Make an intance of the recommender
         recommender = MovieRecommender(userId = inputs["user id"])
+
         # Ask a list of recommendations
         print("-----------------------")
         print("Our recommendations are")
         print("-----------------------")
-        print(recommender.recommend(limit = inputs.setdefault("Recommendations limit",4)).to_string())
+
+        print(recommender.recommend(recomendations_limit = "Recommendations limit",4), historical_limit = inputs.setdefault("Historicals limit",10)).to_string())
+
+        '''
         # To compare the above result call for the top historical of ratings for the given user
         print("----------------------")
         print("Because you watched...")
         print("----------------------")
-        print(recommender.get_top_historical_ratings(limit = inputs.setdefault("Historicals limit",10)).to_string())
+        print(recommender.get_top_historical_ratings().to_string())
+        '''
     except:
         print("Something went wrong!")
